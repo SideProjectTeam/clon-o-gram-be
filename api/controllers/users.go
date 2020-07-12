fd53cd2e124aea060962ca7c11e4fbd5423b3bc5
Okan Akdogan <akdogan1258@hotmail.com>
2020-07-12 22:20:31 +0300

    pw hashing added.

--- api/controllers/users.go
+++ api/controllers/users.go
@@ -10,9 +10,13 @@ import (
 	"github.com/SideProjectTeam/clon-o-gram-be/api/mngdb"
 	"github.com/SideProjectTeam/clon-o-gram-be/api/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 )
 
+//TODO add loggers for errors
 //TODO add better validations
+
+
 type CreateUserBody struct {
 	Username string `validate:"required,min=3,max=15"`
 	Password string `validate:"required,min=8"`
@@ -20,11 +24,12 @@ type CreateUserBody struct {
 }
 
 
-
+//GetUsers Dumb method
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("List of users"))
 }
 
+//RegisterUser is controller function for register a user
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 	var body CreateUserBody
@@ -32,26 +37,38 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
+		return
 	}
 
 	if err := validator.New().Struct(body); err != nil {
 		http.Error(w, err.Error(), 400)
+		return
 	}
 
-	// need password hashing
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.MinCost) 
+	if err != nil {
+		fmt.Println(err)
+		return
+    }
 	user := &models.User{
 		ID: primitive.NewObjectID(),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 		Username: body.Username,
-		Password: body.Password, 
+		Password: string(hash), 
 		Email: body.Email,
 	}
+
 	//Create
-	res,err := mngdb.Clonodb.Collection("Users").InsertOne(mngdb.Ctx, user)
-	fmt.Println(res)
+	_,err = mngdb.Clonodb.Collection("Users").InsertOne(mngdb.Ctx, user)
+	if err!=nil{
+		http.Error(w,err.Error(),400)
+		return
+	}
+
 	if err!=nil{
 		fmt.Println(err)
 	}
 
+	fmt.Fprintf(w,"User Created %s",body.Username)
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/leodido/go-urn v1.2.0 // indirect
 	go.mongodb.org/mongo-driver v1.3.4
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 
